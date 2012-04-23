6728c5fdbc648a47462e3838d7aaaf69a9fce0fa
Florent Odier <fod@zenexity.com>
2012-04-23 15:10:46 +0200

    [Evo] WS for getting connected user's name

--- BackOfficeNG/grails-app/conf/ServiceUrlMappings.groovy
+++ BackOfficeNG/grails-app/conf/ServiceUrlMappings.groovy
@@ -12,5 +12,6 @@ class ServiceUrlMappings {
     "/service/request/$requestId/state" (controller : "serviceRequestExternal"){action = [POST : "requestState"]}
     "/service/requestType/$requestTypeLabel/season" (controller : "serviceRequestExternal"){action = [GET : "requestTypeSeason"]}
     "/service/templates/$filename?" (controller : "localAuthorityResource"){action = [PUT : "template"]}
+    "/service/login" (controller : "serviceUser"){action = [GET : "login"]}
   }
 }
