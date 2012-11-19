4dc9ae806558728c14a3e978169e40e9baecf48c
Benoit Orihuela <bor@zenexity.com>
2012-11-19 08:49:27 +0100

    [Fix][Core] Enums crap in RSR and HCCR

---
+++ Model/db/updates/4.7/core/003-4.7-core-enums_crap.sql
@@ -0,0 +1,3 @@
+UPDATE remote_support_request SET request_information_request_kind = 'INDIVIDUAL'  Where request_information_request_kind = 'Individual';
+UPDATE handicap_compensation_child_request set subject_parental_authority_holder = 'FATHER' where subject_parental_authority_holder = 'Father';
+
