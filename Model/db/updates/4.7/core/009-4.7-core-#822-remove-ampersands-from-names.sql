c0e425a2f99e98be6142a496f0d3ac247922184e
Adrien Maillol <ama@zenexity.com>
2013-05-25 15:17:27 +0200

    [Fix][SQL] #822 Remove '&' from first and last names

---
+++ Model/db/updates/4.7/core/009-4.7-core-#822-remove-ampersands-from-names.sql
@@ -0,0 +1,4 @@
+update individual set last_name = REPLACE(last_name, '&', ' ');
+update individual set first_name = REPLACE(first_name, '&', ' ');
+update individual set first_name_2 = REPLACE(first_name_2, '&', ' ');
+update individual set first_name_3 = REPLACE(first_name_3, '&', ' ');
