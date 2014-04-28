e81b668480dcf9814b7f53d7e6a57ef23b81f8b0
Adrien Maillol <ama@zenexity.com>
2014-04-28 14:42:59 +0200

    [Fix] #9820 Update form on mean of contact change

--- BackOfficeNG/web-app/js/backoffice/contact.js
+++ BackOfficeNG/web-app/js/backoffice/contact.js
@@ -69,9 +69,15 @@ zenexity.capdemat.tools.namespace('zenexity.capdemat.bong');
         }, true);
         zcb.Contact.clickEv = new zct.Event(zcb.Contact, zcb.Contact.processClick);
         yue.on(container, "click", zcb.Contact.clickEv.dispatch, zcb.Contact.clickEv, true);
+        yue.on(container, "change", zcb.Contact.clickEv.dispatch, zcb.Contact.clickEv, true);
       },
       processClick : function(e) {
-        return (yue.getTarget(e).id||'_').split('_')[0];
+        var tgt = yue.getTarget(e);
+        if (tgt.options) {
+          return tgt.options[0].id.split('_')[0];
+        } else {
+          return (yue.getTarget(e).id || '_').split('_')[0];
+        }
       },
       switchMoC : function(e) {
         zcb.Contact.switchField("recipient");
